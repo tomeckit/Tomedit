1ac7626fc71a3885319bbbce2d2fcad050d276c6
tomeckit <a1986@o2.pl>
2017-06-25 20:13:43 +0200

    dodano nowe kontrolki

--- tomedit.py
+++ tomedit.py
@@ -1,4 +1,5 @@
-﻿# -*- coding: utf-8 -*-
+﻿
+# -*- coding: utf-8 -*-
 
 
 import wx
@@ -7,13 +8,16 @@ class MainWindow(wx.Frame):
  def __init__(self, parent):
   wx.Frame.__init__(self, parent, title="Tomedit")
   self.initUi()
+
   self.bindEvents()
   self.setDefaultValues()
   self.Maximize(True)
+
  def initUi(self):
   self.ctrls={}
-  self.staticBoxCtrls = {}
+  self.volumeboxctrls = {}
   self.volumeMethodCtrls={}
+  self.toneboxctrls={}
   panel = wx.Panel(self)
   sizer = wx.BoxSizer(wx.VERTICAL)
 
@@ -21,6 +25,8 @@ class MainWindow(wx.Frame):
   self.ctrls['list'] = wx.ListView(panel)
   self.ctrls['list'].InsertColumn(0, 'nazwa pliku')
   self.ctrls['list'].InsertColumn(1, 'ścieżka')
+  self.ctrls['listLabel'] = wx.StaticText(panel, label='pliki')
+
   self.ctrls['addFile'] = wx.Button(panel, label='dodaj plik')
   self.ctrls['addFolder'] = wx.Button(panel, label='dodaj folder')
   self.ctrls['subFolders'] = wx.CheckBox(panel,label='uwzględniaj podfoldery')
@@ -31,28 +37,45 @@ class MainWindow(wx.Frame):
   self.ctrls['fadeoutLabel'] = wx.StaticText(panel, label='Ms')
   self.ctrls['fadeoutValue'] = wx.SpinCtrl(panel, value='0', min=0, max=20000)
   self.ctrls['changeVolume'] = wx.CheckBox(panel,label='Zmień głośność')
-  self.staticBoxCtrls['volumeChangeGroup'] = wx.StaticBox( panel, -1, "Zmiana głośności" )
-  self.ctrls['staticBoxSizer'] = wx.StaticBoxSizer(self.staticBoxCtrls['volumeChangeGroup'], wx.VERTICAL)
-  self.staticBoxCtrls['volumeMethodManual'] = wx.RadioButton( panel, -1, " Ręcznie ", style = wx.RB_GROUP )
-  self.staticBoxCtrls['volumeMethodAuto'] = wx.RadioButton( panel, -1, " Automatycznie " )
-
-  self.staticBoxCtrls['volumeLabel'] = wx.StaticText(panel, label='DB')
-
-  self.staticBoxCtrls['volumeValue'] = wx.SpinCtrl(panel, value='0', min=-140, max=140)
-
+  self.volumeboxctrls['volumeChangeGroup'] = wx.StaticBox( panel, -1, "Zmiana głośności" )
+  self.ctrls['volumeBoxSizer'] = wx.StaticBoxSizer(self.volumeboxctrls['volumeChangeGroup'], wx.VERTICAL)
+  self.volumeboxctrls['volumeMethodManual'] = wx.RadioButton( panel, -1, " Ręcznie ", style = wx.RB_GROUP )
+  self.volumeboxctrls['volumeMethodAuto'] = wx.RadioButton( panel, -1, " Automatycznie " )
+  self.volumeboxctrls['volumeLabel'] = wx.StaticText(panel, label='DB')
+  self.volumeboxctrls['volumeValue'] = wx.SpinCtrl(panel, value='0', min=-140, max=140)
   self.ctrls['limiter'] = wx.CheckBox(panel,label='nie dopuszczaj do przesterowania')
-  self.ctrls['swapChannels'] = wx.CheckBox(panel,label='Zamień kanały')
-  self.ctrls['start'] = wx.Button(panel, label='Start')
-  self.volumeMethodCtrls['volumeMethodAuto'] = self.staticBoxCtrls['volumeMethodAuto']
-  self.volumeMethodCtrls['volumeMethodManual'] = self.staticBoxCtrls['volumeMethodManual']
 
-  for ctrl in self.staticBoxCtrls.iterkeys():
-   if ctrl != 'volumeChangeGroup': self.ctrls['staticBoxSizer'].Add(self.staticBoxCtrls[ctrl])
+  self.ctrls['swapChannels'] = wx.CheckBox(panel,label='Zamień kanały')
+
+  self.ctrls['changeTone'] = wx.CheckBox(panel,label='Zmień barwę')
+  self.toneboxctrls['toneChangeGroup'] = wx.StaticBox( panel, -1, "Zmiana barwy" )
+  self.ctrls['toneBoxSizer'] = wx.StaticBoxSizer(self.toneboxctrls['toneChangeGroup'], wx.VERTICAL)
+  self.toneboxctrls['toneloLabel'] = wx.StaticText(panel, label='Bas')
+  self.toneboxctrls['toneloValue'] = wx.SpinCtrl(panel, value='0', min=-12, max=12)
+  self.toneboxctrls['tonehiLabel'] = wx.StaticText(panel, label='Góra')
+  self.toneboxctrls['tonehiValue'] = wx.SpinCtrl(panel, value='0', min=-12, max=12)
+  self.ctrls['convert'] = wx.CheckBox(panel,label='Konwertuj')
+  self.formats = ['mp3', 'ogg', 'flac', 'wma', 'aac LC', 'mpc', 'opus']
+  self.ctrls['convertlistLabel'] = wx.StaticText(panel, label='Konwertuj do')
+  self.ctrls['convertList'] = wx.Choice(panel, choices =self.formats)
+
+  self.ctrls['start'] = wx.Button(panel, label='Start')
+  self.volumeMethodCtrls['volumeMethodAuto'] = self.volumeboxctrls['volumeMethodAuto']
+  self.volumeMethodCtrls['volumeMethodManual'] = self.volumeboxctrls['volumeMethodManual']
+
+  for ctrl in self.volumeboxctrls.iterkeys():
+   if ctrl != 'volumeChangeGroup': self.ctrls['volumeBoxSizer'].Add(self.volumeboxctrls[ctrl])
+
+  for ctrl in self.toneboxctrls.iterkeys():
+   if ctrl != 'toneChangeGroup': self.ctrls['toneBoxSizer'].Add(self.toneboxctrls[ctrl])
 
   for ctrl in self.ctrls.itervalues():
    sizer.Add(ctrl)
-  for ctrl in self.staticBoxCtrls.iterkeys():
-   self.ctrls[ctrl] = self.staticBoxCtrls[ctrl]
+  for ctrl in self.volumeboxctrls.iterkeys():
+   self.ctrls[ctrl] = self.volumeboxctrls[ctrl]
+  for ctrl in self.toneboxctrls.iterkeys():
+   self.ctrls[ctrl] = self.toneboxctrls[ctrl]
+
 
   panel.SetSizerAndFit(sizer)
 
@@ -64,7 +87,8 @@ class MainWindow(wx.Frame):
   self.ctrls['changeVolume'].Bind(wx.EVT_CHECKBOX, self.onChangeVolume)
   self.ctrls['volumeMethodManual'].Bind(wx.EVT_RADIOBUTTON, self.onManual)
   self.ctrls['volumeMethodAuto'].Bind(wx.EVT_RADIOBUTTON, self.onAuto)
-
+  self.ctrls['changeTone'].Bind(wx.EVT_CHECKBOX, self.onChangeTone)
+  self.ctrls['convert'].Bind(wx.EVT_CHECKBOX, self.onconvert)
 
  def onFadein(self, e):
   checkbox = e.GetEventObject()
@@ -76,7 +100,7 @@ class MainWindow(wx.Frame):
 
  def onChangeVolume(self, e):
   checkbox = e.GetEventObject()
-  for ctrl in self.staticBoxCtrls.itervalues():
+  for ctrl in self.volumeboxctrls.itervalues():
    ctrl.Enable(checkbox.GetValue())
 
  def onManual(self, e):
@@ -84,6 +108,15 @@ class MainWindow(wx.Frame):
 
  def onAuto(self, e):
   self.ctrls['volumeValue'].Enable(False)
+
+ def onChangeTone(self, e):
+  checkbox = e.GetEventObject()
+  for ctrl in self.toneboxctrls.itervalues():
+   ctrl.Enable(checkbox.GetValue())
+
+ def onconvert(self, e):
+  checkbox = e.GetEventObject()
+  self.ctrls['convertList'].Enable(checkbox.GetValue())
 
 
  def onAddFile(self, e):
@@ -94,8 +127,11 @@ class MainWindow(wx.Frame):
  def setDefaultValues(self):
   self.ctrls['fadeinValue'].Enable(self.ctrls['fadein'].GetValue())
   self.ctrls['fadeoutValue'].Enable(self.ctrls['fadeout'].GetValue())
-  for ctrl in self.staticBoxCtrls.itervalues():
+  for ctrl in self.volumeboxctrls.itervalues():
    ctrl.Enable(self.ctrls['changeVolume'].GetValue())
+  for ctrl in self.toneboxctrls.itervalues():
+   ctrl.Enable(self.ctrls['changeTone'].GetValue())
+  self.ctrls['convertList'].Enable(self.ctrls['convert'].GetValue())
 
 class Controller:
  def __init__(self, app):
